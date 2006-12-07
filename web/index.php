ccc54474e4ef2978fb22a03e834ea2b52f2cfad1
louise <louise>
2006-12-07 15:55:17 +0000

    Added better text

--- web/index.php
+++ web/index.php
@@ -6,25 +6,25 @@
  * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
  * Email: louise.crow@gmail.com. WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.1 2006-12-05 13:25:18 louise Exp $
+ * $Id: index.php,v 1.2 2006-12-07 15:55:17 louise Exp $
  * 
  */
 
 require_once "../../phplib/utility.php";
 require_once "../phplib/news.php";
 require_once "../phplib/page.php";
-
-page_header("Index page for news demo");
-news_general_heading();
+$heading = "Contact your local newspaper";
+page_header("NeWs - " . $heading);
+print h2($heading);
 index_page();
 page_footer();
 
 function index_page(){
 ?>
-<form id="search" accept-charset="utf-8" action="/search" method="get">
-<p><label for="q"><?=_('Search') ?>:</label>
+<form id="search" accept-charset="utf-8" action="/search" method="get"><div id="search-box">
+<p><label for="q"><?=_('Type your town, postcode or the name of the paper you want to find') ?>:</label>
 <input type="text" id="q" name="q" size="25" value=""> <input type="submit" value="<?=_('Go') ?>"></p>
-</form>
+</div></form>
 
 <?
 }
