0f5e4e6851db50e780e4d660d31802bf9654736f
richard <richard>
2008-11-22 20:52:18 +0000

    all map

--- docs/mapall.php
+++ docs/mapall.php
@@ -8,13 +8,12 @@ class mapall_page extends pagebase {
 	//bind
 	function bind() {
 
-
         $search = factory::create('search');
 		$groups = $search->search('group', array(
-			array('long_bottom_left', '<', 1),
-			array('long_top_right', '>', -2),
-			array('lat_bottom_left', '<', 55),
-			array('lat_top_right', '>', 50)	
+			array('long_bottom_left', '<', 1.7666667),
+			array('long_top_right', '>', -8.1),
+			array('lat_bottom_left', '<', 60.7833333),
+			array('lat_top_right', '>', 49.1738889)	
 			),
 			'AND'
 		);
