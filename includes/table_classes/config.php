c3ab32322c3f4a857d50a5a9f8ff3e165c7554fd
francis <francis>
2010-01-28 00:40:36 +0000

    Use our standard name for DB configs

--- includes/table_classes/config.php
+++ includes/table_classes/config.php
@@ -4,18 +4,18 @@
 
 	$debug_level = SQL_DEBUG_LEVEL;
 	if(isset($_GET['sqldebug'])){
-		$debug_level = $_GET['sqldebug'];
+		//$debug_level = $_GET['sqldebug'];
 	}
 
 
 	$options = array(
-	'database' => 'mysql://' . DB_USER . ':'. DB_PASS .'@' . DB_HOST . '/' . DB_NAME,
+	'database' => 'mysql://' . OPTION_GNY_DB_USER . ':'. OPTION_GNY_DB_PASS .'@' . OPTION_GNY_DB_HOST . '/' . OPTION_GNY_DB_NAME,
 	'schema_location' => INCLUDE_DIR . '/table_classes/',
 	'class_location' => INCLUDE_DIR . '/table_classes/',
 	'require_prefix' => 'DataObjects/',
 	'class_prefix' => 'tableclass_',
 	'db_driver' => 'MDB2',
-	'ini_' . DB_NAME => INCLUDE_DIR . '/table_classes/groups.ini',	
+	'ini_' . OPTION_GNY_DB_NAME => INCLUDE_DIR . '/table_classes/groups.ini',	
 	"debug" => $debug_level
 	);
 ?>
